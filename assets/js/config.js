4c91934ee12803ba67a0ae5e720f4d0aabd37090
Jonathan Manes <manesjona@gmail.com>
2022-12-06 11:32:50 +0100

    renad image folder

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@ export const DISHES = [
         id: "jambon-fromage",
         name: "JAMBON & FROMAGE",
         price: 11.95,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_jambon_fromage.webp",
         category: [],
         description: "Jambon belge fermier, Gouda bio, cornichons bio, sauce César et persil plat"
     },
@@ -14,7 +14,7 @@ export const DISHES = [
         id: "poulet-courgette",
         name: "POULET & COURGETTE",
         price: 14.95,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_poulet_courgette.webp",
         category: [],
         description: "Poulet fermier, courgettes grillées, sauce aux herbes, coriandre et parmesan"
     },
@@ -25,7 +25,7 @@ export const DISHES = [
         id: "saumon-fume",
         name: "SAUMON FUMÉ",
         price: 16.95,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_saumon.webp",
         category: [],
         description: "Saumon Atlantique fumé, pickles de concombre, radis, sauce aux herbes, aneth et ciboulette"
     },
@@ -35,7 +35,7 @@ export const DISHES = [
         id: "tartare-boeuf",
         name: "TARTARE DE BŒUF BELGE",
         price: 14.95,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_tartare.webp",
         category: [],
         description: "Boeuf belge bio, parmesan, huile de basilic et tomates séchées bio"
     },
@@ -45,7 +45,7 @@ export const DISHES = [
         id: "thon",
         name: "THON",
         price: 12.45,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_thon.webp",
         category: [],
         description: "Thon, houmous bio, céléri, ciboulette, radis et pickles de concombre"
     },
@@ -55,7 +55,7 @@ export const DISHES = [
         id: "italienne",
         name: "ITALIENNE",
         price: 15.45,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_italienne.webp",
         category: [],
         description: "Jambon sec, mozzarella di bufala bio, pesto rosso bio, tomates cerises et huile de basilic"
     },
@@ -65,7 +65,7 @@ export const DISHES = [
         id: "toast-avocat",
         name: "TOAST AVOCAT",
         price: 12.45,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/pain-2.webp",
         category: ["vegan"],
         description: "Pain de blé au levain bio toasté avec guacamole, pickles de concombre, radis, super graines bio et sel citron-cumin"
     },
@@ -75,7 +75,7 @@ export const DISHES = [
         id: "croque-monsieur",
         name: "CROQUE MONSIEUR",
         price: 16.95,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine-croque.webp",
         category: [],
         description: "Pain de blé au levain bio toasté (double) avec jambon fermier belge, Gouda bio et servi avec sauce tomate bio et salade mixte"
     },
@@ -85,7 +85,7 @@ export const DISHES = [
         id: "gardener-croque",
         name: "GARDENER’S CROQUE",
         price: 15.95,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine-gardner-s-croque.webp",
         category: ["vegan"],
         description: "Pain de blé au levain bio toasté avec houmous bio, courgettes grillées, " +
             "pesto rosso bio servi avec salade mixte, avocat, super graines bio, vinaigrette vegan et salsa tomate bio"
@@ -96,7 +96,7 @@ export const DISHES = [
         id: "toast-poulet",
         name: "TOAST POULET & MOZZARELLA FUMÉE",
         price: 16.45,
-        picture: "assets/image/sandwich.webp",
+        picture: "assets/image/tartine_poulet_mozza.webp",
         category: [],
         description: "pain de blé au levain bio toasté avec poulet fermier, "
             + " mozzarella fumée, courgettes grillées, huile de basilic et persil"
